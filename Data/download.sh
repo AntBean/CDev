833f695dff16f11a4c6784d6e6811ba2bb788733
zhaojunbo <zhaojunbo1992chasing@gmail.com>
2015-06-07 17:53:41 -0400

    Data downloading done.

---
+++ Data/download.sh
@@ -0,0 +1,8 @@
+wget --load-cookies cookies.txt https://www.kaggle.com/c/icdm-2015-drawbridge-cross-device-connections/download/dev_train_basic.csv.zip -P ../Save/Data/
+wget --load-cookies cookies.txt https://www.kaggle.com/c/icdm-2015-drawbridge-cross-device-connections/download/cookie_all_basic.csv.zip -P ../Save/Data/
+wget --load-cookies cookies.txt https://www.kaggle.com/c/icdm-2015-drawbridge-cross-device-connections/download/ipagg_all.csv.zip -P ../Save/Data/
+wget --load-cookies cookies.txt https://www.kaggle.com/c/icdm-2015-drawbridge-cross-device-connections/download/dev_test_basic.csv.zip -P ../Save/Data/
+wget --load-cookies cookies.txt https://www.kaggle.com/c/icdm-2015-drawbridge-cross-device-connections/download/property_category.csv.zip -P ../Save/Data/
+wget --load-cookies cookies.txt https://www.kaggle.com/c/icdm-2015-drawbridge-cross-device-connections/download/sampleSubmission.csv.zip -P ../Save/Data/
+wget --load-cookies cookies.txt https://www.kaggle.com/c/icdm-2015-drawbridge-cross-device-connections/download/id_all_ip.csv.zip -P ../Save/Data/
+wget --load-cookies cookies.txt https://www.kaggle.com/c/icdm-2015-drawbridge-cross-device-connections/download/id_all_property.csv.zip -P ../Save/Data/
