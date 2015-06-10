5f9e17ce8f720d2ee9e18401866c3eb0baae247c
jakezhaojb <zhaojunbo1992chasing@gmail.com>
2015-06-09 22:09:54 -0400

    Added config file

---
+++ base/config.py
@@ -0,0 +1,27 @@
+#-*- coding: utf-8 -*-
+"""
+config file.
+By Jake Zhao
+"""
+
+import os
+from collections import defaultdict
+
+config = defaultdict(defaultdict)
+
+# Data
+config['data']['trainpath'] = 'path/to/train'
+config['data']['testpath'] = 'path/to/test'
+
+
+# model
+config['model']['name'] = 'lr'
+
+# train
+config['train'] = {}
+
+# test
+config['test']['flag'] = True
+
+# main
+config['main']['save'] = 'path/to/save'
---
+++ base/data.py
@@ -0,0 +1,5 @@
+#-*- coding: utf-8 -*-
+"""
+Data file.
+By Stephen Lai
+"""
