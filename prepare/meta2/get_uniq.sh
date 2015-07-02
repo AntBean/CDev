9bf4957e0d2104973757e2bcc995784a58b4ef14
jakezhaojb <zhaojunbo1992chasing@gmail.com>
2015-07-02 00:09:09 -0400

    Done get uniq

--- prepare/meta2/get_uniq.sh
+++ prepare/meta2/get_uniq.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-# dev_train_basic.csv
-idx="1 2 3 4 5 7 8"
-for i in `echo $idx`
-do
-    awk '{split ($0,a,","); print a['"$i"']}' ../../DataSample/dev_train_basic.csv | uniq 
-    # Next, pipe and seirielize to a python list, passing to the next stage python handle
-done
+awk '{split ($0,a,","); print a['"$1"']}' $2 | uniq  | grep -v '^\-1'
