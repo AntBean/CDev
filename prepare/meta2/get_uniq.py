7e0aa6297f97d950f8d768132474e04a98404332
jakezhaojb <zhaojunbo1992chasing@gmail.com>
2015-07-02 00:57:24 -0400

    done python control code

--- prepare/meta2/get_uniq.py
+++ prepare/meta2/get_uniq.py
@@ -2,7 +2,6 @@
 # By Jake
 
 import os
-import sys
 import pickle
 import subprocess
 
@@ -15,7 +14,27 @@ g_save_path = '../../DataSample'
 
 def get_uniq_column(fn, idx):
     '''opens a sub-thread to get uniq strings'''
-    p = subprocess.Popen(['./get_uniq.sh', str(idx) + ' ' + fn],
+    p = subprocess.Popen(['./get_uniq.sh', str(idx), fn],
                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
+    out = out.split('\n')
+    out.pop()  # last one is usually empty string
     return out
+
+
+def main():
+    uniq_list = {'dev': {}, 'coo': {}}
+    for idx in g_index_dummy.get('dev'):
+        uniq_list['dev'][idx] = get_uniq_column(os.path.join(g_load_path,
+                                                'dev_train_basic.csv'), idx)
+    for idx in g_index_dummy.get('coo'):
+        uniq_list['coo'][idx] = get_uniq_column(os.path.join(g_load_path,
+                                                'cookie_all_basic.csv'), idx)
+    # saving
+    save = open(os.path.join(g_save_path, 'uniq.pkl'), 'w')
+    pickle.dump(uniq_list, save)
+    save.close()
+
+
+if __name__ == '__main__':
+    main()
