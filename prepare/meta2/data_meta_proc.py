95e734beb86c48d82a101a7ed342fdf70e29d098
zhaojunbo <zhaojunbo1992chasing@gmail.com>
2015-07-03 23:46:28 -0400

    lots of bug fixed

--- prepare/meta2/data_meta_proc.py
+++ prepare/meta2/data_meta_proc.py
@@ -1,9 +1,6 @@
 # -*- coding:utf-8-*-
 # By Jake
 
-# TODO
-# header
-
 import os
 import numpy as np
 from scipy import sparse
@@ -13,7 +10,7 @@ import pickle
 
 # Some global variables
 g_index_dummy = {'dev': [2, 3, 4, 5, 7, 8],
-                 'coo': [2, 3, 4, 5, 7]}
+                 'coo': [2, 3, 4, 5, 7, 8]}
 g_load_path = '../../DataSample'
 g_save_path = '../../DataSample/2015.07.02.primitive'
 g_num_neg = 0
@@ -25,7 +22,7 @@ def load_as_dataframe(fn):
     return df
 
 
-def add_dummies(ds, idx, ref):
+def add_dummies(ds, idx, ref, label=False):
     """convert a data series to an array with dummy variables"""
 
     def to_dummies(string, ref):
@@ -36,32 +33,49 @@ def add_dummies(ds, idx, ref):
         arr[idx] = 1
         return arr
 
-    assert len(ref) == len(ds)  # references have to be padded
+    assert len(ref) == len(ds)-1  # references have to be padded, and drawbridge_handle is the only diff
     assert isinstance(ds, pd.core.series.Series)
     assert isinstance(idx, list)
     arr = ds.values.copy()
+    # completeness examining
+    incomplete = False
+    for elem in arr:
+        incomplete = incomplete or elem=='-1'
+    if incomplete:
+        return
+    offset_dummies = 0
+    # converting
     for idx_elem in idx:
-        raw = arr[idx_elem]
-        arr = np.delete(arr, idx_elem)
-        np.insert(arr, idx_elem, to_dummies(raw, ref[idx_elem]))
+        idx_elem_offset = idx_elem + offset_dummies
+        raw = arr[idx_elem_offset]
+        arr = np.delete(arr, idx_elem_offset)
+        dummies = to_dummies(raw, ref[idx_elem])
+        arr = np.insert(arr, idx_elem_offset, dummies)
+        offset_dummies += len(dummies) - 1
+    if not label:
+        arr = arr[1:]
     return arr
 
 
-def padding(idx, ref, pad=[]):
+def padding(num, ref, pad=[]):
     """padding the references"""
-    assert isinstance(ref, list)
-    assert isinstance(idx, list)
-    for i, _ in enumerate(ref.keys()):
-        if i not in idx:
-            ref[i] = pad
+    assert isinstance(ref, dict)
+    assert isinstance(num, int)
+    diff = list( set(range(num)) - set(ref.keys()) ) 
+    for i in diff:
+        ref[i] = pad 
+    return ref
 
 
 def get_match_idx(df1, df2, kw='drawbridge_handle'):
     """matching"""
     matched_idx = []
     for id1, handle in enumerate(df1[kw]):
-        id2 = np.where(df2[kw])[0].tolist()
-        matched_idx.append(map(lambda x: (id1, x), id2))
+        id2 = np.where(df2[kw] == handle)[0].tolist()
+        if len(id2) == 0:
+            continue
+        for x in id2:
+            matched_idx.append( (id1, x) )
     return matched_idx
 
 
@@ -76,42 +90,84 @@ def get_unmatch_idx(df1, df2, num, kw='drawbridge_handle'):
     return unmatched_idx
 
 
+def prune_lil_matrix(mat, idx):
+    """prune a lil matrix one row, i.e. No idx-th row"""
+    assert isinstance(mat, sparse.lil_matrix)
+    mat.rows = np.delete(mat.rows, idx)
+    mat.data = np.delete(mat.data, idx)
+    mat._shape = (mat._shape[0] - 1, mat._shape[1])
+    return mat
+
+
+def prune_allzero_row_lil_matrix(mat):
+    """find and prune all zero rows in lil_matrix"""
+    rowidx, _ = mat.nonzero()
+    for relem in rowidx:
+        mat = prune_lil_matrix(mat, relem)
+    return mat
+
+
 def main():
-    print('Start generating positive dataset')
     dev_df = load_as_dataframe(os.path.join(g_load_path, 'dev_train_basic.csv'))
     coo_df = load_as_dataframe(os.path.join(g_load_path, 'cookie_all_basic.csv'))
     # uniq.pkl load
-    uniq_ref = pickle.load(open(os.path.join(g_load_path, 'uniq.pkl')))
-    uniq_ref_dev = padding(g_index_dummy.get('dev'), uniq_ref.get('dev'))
-    uniq_ref_coo = padding(g_index_dummy.get('coo'), uniq_ref.get('coo'))
+    uniq_ref = pickle.load(open(os.path.join(g_load_path, 'uniq.pkl'), 'rb'))
+    uniq_ref_dev = padding(len(dev_df.columns)-1, uniq_ref.get('dev'))
+    uniq_ref_coo = padding(len(coo_df.columns)-1, uniq_ref.get('coo'))
+    # adjust g_index_dummy
+    g_index_dummy['dev'] = list( map(lambda x: x-1, g_index_dummy.get('dev')) )
+    g_index_dummy['coo'] = list( map(lambda x: x-1, g_index_dummy.get('coo')) )
     # example generation, to get length
-    dev_df_dummy_fill = add_dummies(dev_df.iloc[0],
-                                    g_index_dummy.get('dev'),
-                                    uniq_ref_dev)
-    coo_df_dummy_fill = add_dummies(coo_df.iloc[0],
-                                    g_index_dummy.get('coo'),
-                                    uniq_ref_coo)
-    if g_num_neg <= 0:
-        g_num_neg = len(matched_idx)
+    length_dev, length_coo = 0, 0
+    while length_dev == 0:
+        dev_df_dummy_fill = add_dummies(dev_df.iloc[0],
+                                        g_index_dummy.get('dev'),
+                                        uniq_ref_dev)
+        length_dev = len(dev_df_dummy_fill) if dev_df_dummy_fill is not None else 0
+    while length_coo == 0:
+        coo_df_dummy_fill = add_dummies(coo_df.iloc[0],
+                                        g_index_dummy.get('coo'),
+                                        uniq_ref_coo)
+        length_coo = len(coo_df_dummy_fill) if coo_df_dummy_fill is not None else 0
     # start matching and unmatching
     matched_idx = get_match_idx(dev_df, coo_df)
+    global g_num_neg
+    if g_num_neg <= 0:
+        g_num_neg = len(matched_idx)
     unmatched_idx = get_unmatch_idx(dev_df, coo_df, g_num_neg)
     # pos and neg data initialization
-    pos_data = {'dev': sparse.csr_matrix((len(matched_idx), len(dev_df_dummy_fill))),
-                'coo': sparse.csr_matrix((len(matched_idx), len(coo_df_dummy_fill)))}
-    neg_data = {'dev': sparse.csr_matrix((g_num_neg, len(dev_df_dummy_fill))),
-                'coo': sparse.csr_matrix((g_num_neg, len(coo_df_dummy_fill)))}
+    import pdb; pdb.set_trace()
+    pos_data = {'dev': sparse.lil_matrix((len(matched_idx), length_dev)),
+                'coo': sparse.lil_matrix((len(matched_idx), length_coo))}
+    neg_data = {'dev': sparse.lil_matrix((g_num_neg, len(dev_df_dummy_fill))),
+                'coo': sparse.lil_matrix((g_num_neg, len(coo_df_dummy_fill)))}
     # start filling pos/neg data
     for i, (x, y) in enumerate(matched_idx):
-        pos_data['dev'][i] = add_dummies(dev_df.iloc[x])
-        pos_data['coo'][i] = add_dummies(coo_df.iloc[x])
+        pos_dev_elem = add_dummies(dev_df.iloc[x], g_index_dummy.get('dev'), uniq_ref_dev)
+        pos_coo_elem = add_dummies(coo_df.iloc[y], g_index_dummy.get('coo'), uniq_ref_coo)
+        if pos_dev_elem is not None and pos_coo_elem is not None:
+            continue
+        pos_data['dev'][i] = pos_dev_elem
+        pos_data['coo'][i] = pos_coo_elem
     print('positive data generating done.')
     for i, (x, y) in enumerate(unmatched_idx):
-        neg_data['dev'][i] = add_dummies(dev_df.iloc[x])
-        neg_data['coo'][i] = add_dummies(coo_df.iloc[x])
+        neg_dev_elem = add_dummies(dev_df.iloc[x], g_index_dummy.get('dev'), uniq_ref_dev)
+        neg_coo_elem = add_dummies(coo_df.iloc[y], g_index_dummy.get('coo'), uniq_ref_coo)
+        if neg_dev_elem is not None and neg_coo_elem is not None:
+            continue
+        neg_data['dev'][i] = neg_dev_elem
+        neg_data['coo'][i] = neg_coo_elem
     print('negitive data generating done.')
+    # pruning the all-zero line
+    prune_allzero_row_lil_matrix(pos_data['dev'])
+    prune_allzero_row_lil_matrix(pos_data['coo'])
+    prune_allzero_row_lil_matrix(neg_data['dev'])
+    prune_allzero_row_lil_matrix(neg_data['coo'])
+    # unit-test
+    assert(pos_data['dev'].shape[0] == pos_data['coo'].shape[0])
+    assert(neg_data['dev'].shape[0] == neg_data['coo'].shape[0])
+    print('No assertion is raised.')
     # saving
-    # TODO remain a problem, I want to get a panacea here to avoid memory filling; that is, directly dumping csr_matrix to .csv file.
 
 
 
--- prepare/meta2/get_uniq.py
+++ prepare/meta2/get_uniq.py
@@ -7,7 +7,7 @@ import subprocess
 
 # global variables
 g_index_dummy = {'dev': [2, 3, 4, 5, 7, 8],
-                 'coo': [2, 3, 4, 5, 7]}
+                 'coo': [2, 3, 4, 5, 7, 8]}
 g_load_path = '../../DataSample'
 g_save_path = '../../DataSample'
 
@@ -17,6 +17,7 @@ def get_uniq_column(fn, idx):
     p = subprocess.Popen(['./get_uniq.sh', str(idx), fn],
                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
+    out = out.decode()
     out = out.split('\n')
     out = out[1:]  # remove the first one element, which is the header
     out.pop()  # last one is usually empty string
@@ -26,13 +27,13 @@ def get_uniq_column(fn, idx):
 def main():
     uniq_list = {'dev': {}, 'coo': {}}
     for idx in g_index_dummy.get('dev'):
-        uniq_list['dev'][idx] = get_uniq_column(os.path.join(g_load_path,
+        uniq_list['dev'][idx-1] = get_uniq_column(os.path.join(g_load_path,
                                                 'dev_train_basic.csv'), idx)
     for idx in g_index_dummy.get('coo'):
-        uniq_list['coo'][idx] = get_uniq_column(os.path.join(g_load_path,
+        uniq_list['coo'][idx-1] = get_uniq_column(os.path.join(g_load_path,
                                                 'cookie_all_basic.csv'), idx)
     # saving
-    save = open(os.path.join(g_save_path, 'uniq.pkl'), 'w')
+    save = open(os.path.join(g_save_path, 'uniq.pkl'), 'wb')
     pickle.dump(uniq_list, save)
     save.close()
 
