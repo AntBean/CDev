f5703b4ab81b8ce7c15c1f6490a3d244e9c5730f
zhaojunbo <zhaojunbo1992chasing@gmail.com>
2015-07-03 21:30:26 -0400

    bug fixed of the header

--- prepare/meta2/get_uniq.py
+++ prepare/meta2/get_uniq.py
@@ -18,6 +18,7 @@ def get_uniq_column(fn, idx):
                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
     out = out.split('\n')
+    out = out[1:]  # remove the first one element, which is the header
     out.pop()  # last one is usually empty string
     return out
 
