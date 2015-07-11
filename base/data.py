a12da99d981890a2f82a971b2c9edb16949e8d2f
jakezhaojb <zhaojunbo1992chasing@gmail.com>
2015-07-10 21:26:22 -0400

    Added data.py in base code

--- base/data.py
+++ base/data.py
@@ -16,9 +16,17 @@ g_num_neg = 0
 
 """
  Some aux functions defined first
+ - load_as_dataframe
  - add_dummies
  - padding
 """
+
+
+def load_as_dataframe(fn):
+    """load csv file into memory"""
+    df = pd.read_csv(fn)
+    return df
+
 
 def add_dummies(ds, idx, ref, label=False):
     """convert a data series to an array with dummy variables"""
@@ -59,10 +67,11 @@ def padding(num, ref, pad=[]):
     """padding the references"""
     assert isinstance(ref, dict)
     assert isinstance(num, int)
-    diff = list( set(range(num)) - set(ref.keys()) ) 
+    diff = list( set(range(num)) - set(ref.keys()) )
     for i in diff:
-        ref[i] = pad 
+        ref[i] = pad
     return ref
+
 
 """
  Main functionality
@@ -70,26 +79,77 @@ def padding(num, ref, pad=[]):
 class DataSource(object):
     """data source class, streaming data samples"""
     def __init__(self, index_dummy, data_path):
+        # args parsing
         self.index_dummy = index_dummy
         self.data_path = data_path
         self.dev_train_data_path = os.path.join(data_path, 'dev_train_basic.csv')
         self.dev_test_data_path = os.path.join(data_path, 'dev_test_basic.csv')
         self.coo_data_path = os.path.join(data_path, 'cookie_all_basic.csv')
+        # load in data
+        self.dev_df_train = load_as_dataframe(self.dev_train_data_path)
+        self.dev_df_test = load_as_dataframe(self.dev_test_data_path)
+        self.coo_df = load_as_dataframe(self.coo_data_path)
+        # get uniq.pkl
+        uniq_ref = pickle.load(open(os.path.join(self.data_path, 'uniq.pkl'), 'rb'))
+        self.uniq_ref_dev = padding(len(dev_df.columns)-1, uniq_ref.get('dev'))
+        self.uniq_ref_coo = padding(len(coo_df.columns)-1, uniq_ref.get('coo'))
+        # get training indices
+        data_idx = pickle.load(open(os.path.join(self.data_path, 'indices.pkl'), 'rb'))
+        self.train_data_idx_pos = data_idx.get('pos')
+        self.train_data_idx_neg = data_idx.get('neg')
 
-    def getSingleSample(self, index, item='dev', set='train'):
+        # get test and validation indices TODO
+
+        # adjust index_dummy
+        self.index_dummy['dev'] = list( map(lambda x: x-1, self.index_dummy.get('dev')) )
+        self.index_dummy['coo'] = list( map(lambda x: x-1, self.index_dummy.get('coo')) )
+        # get the length of dataset
+        length_dev, length_coo = 0, 0
+        while length_dev == 0:
+            id = np.random.randint(len(self.dev_df_train['drawbridge_handle']))
+            dev_df_dummy_fill = add_dummies(self.dev_df_train.iloc[id],
+                                            self.index_dummy.get('dev'),
+                                            self.uniq_ref_dev)
+            length_dev = len(dev_df_dummy_fill) if dev_df_dummy_fill is not None else 0
+        while length_coo == 0:
+            id = np.random.randint(len(self.coo_df['drawbridge_handle']))
+            coo_df_dummy_fill = add_dummies(self.coo_df.iloc[id],
+                                            self.index_dummy.get('coo'),
+                                            self.uniq_ref_coo)
+            length_coo = len(coo_df_dummy_fill) if coo_df_dummy_fill is not None else 0
+        self.length_dev = length_dev
+        self.length_coo = length_coo
+
+    def getNextBatch(self, batch_size, item='dev', set='train'):
+        # parsing
         if item == 'dev':
             if set == 'train':
                 this_path = self.dev_train_data_path
+                this_pos_idx = self.train_data_idx_pos
+                this_neg_idx = self.train_data_idx_neg
             elif set == 'test':
                 this_path = self.dev_test_data_path
+                # TODO
+                print('Not supported so far', file=sys.stderr)
+                sys.exit()
             else:
                 print('set must be [train | test]', file=sys.stderr)
                 sys.exit()
+            this_uniq_ref = self.uniq_ref_dev
         elif item == 'coo':
             this_path = self.coo_data_path
+            this_uniq_ref = self.uniq_ref_coo
         else:
             print('item must be [dev | coo]', file=sys.stderr)
             sys.exit()
+        # data initialization
+        # TODO from here.
+
+
+        # data loading
+
+        for i in range(batch_size):
+            
 
         
         
@@ -97,29 +157,7 @@ class DataSource(object):
 
 
 def main():
-    dev_df = load_as_dataframe(os.path.join(g_load_path, 'dev_train_basic.csv'))
-    coo_df = load_as_dataframe(os.path.join(g_load_path, 'cookie_all_basic.csv'))
-    # uniq.pkl load
-    uniq_ref = pickle.load(open(os.path.join(g_load_path, 'uniq.pkl'), 'rb'))
-    uniq_ref_dev = padding(len(dev_df.columns)-1, uniq_ref.get('dev'))
-    uniq_ref_coo = padding(len(coo_df.columns)-1, uniq_ref.get('coo'))
-    # adjust g_index_dummy
-    g_index_dummy['dev'] = list( map(lambda x: x-1, g_index_dummy.get('dev')) )
-    g_index_dummy['coo'] = list( map(lambda x: x-1, g_index_dummy.get('coo')) )
     # example generation, to get length
-    length_dev, length_coo = 0, 0
-    while length_dev == 0:
-        id = np.random.randint(len(dev_df['drawbridge_handle']))
-        dev_df_dummy_fill = add_dummies(dev_df.iloc[id],
-                                        g_index_dummy.get('dev'),
-                                        uniq_ref_dev)
-        length_dev = len(dev_df_dummy_fill) if dev_df_dummy_fill is not None else 0
-    while length_coo == 0:
-        id = np.random.randint(len(coo_df['drawbridge_handle']))
-        coo_df_dummy_fill = add_dummies(coo_df.iloc[id],
-                                        g_index_dummy.get('coo'),
-                                        uniq_ref_coo)
-        length_coo = len(coo_df_dummy_fill) if coo_df_dummy_fill is not None else 0
     # start matching and unmatching
     matched_idx = get_match_idx(dev_df, coo_df)
     global g_num_neg
